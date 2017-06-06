e86fb6e22b7c98e00ebb584afe679d96cab4bca8
Abdul Haseeb <munawar@lycus.com.pk>
2017-06-06 12:03:49 +0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'app works!';
+
+   public allOption:Array<any> = [ 
+    
+    {id: 1 , name: 'Pakistan'}, {id: 2 , name:'Ballistic'}, 
+    {id: 3 , name:'Abdullah'}, {id: 4 , name:'Zindabad'}
+    
+    ];
+
+
+    getSelectedValue(item) {
+      console.log(item.id +' '+ item.name);
+    }
+
+
+  
 }
---
+++ src/app/components/dropdown.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'drop-down',
+  templateUrl: 'dropdown.component.html',
+  styleUrls: ['dropdown.component.css']
+})
+export class DropDownComponent {
+    // this get the data from the main compoent 
+    @Input() allOption: Array<any> = [];
+    //
+    @Output() public select = new EventEmitter();
+
+    private click(id: any){   
+      console.log(this.allOption.find(n => n.id == id));
+      this.select.emit(this.allOption.find(n => n.id == id));
+    }
+    
+}
